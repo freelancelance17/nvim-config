e299673c27fb77bd40e10bcefbcdec0691003d95
lance knickerbocker <lance@freelancelance.com>
2024-05-28 07:42:02 -0400

    latest

--- init.lua
+++ init.lua
@@ -444,3 +444,20 @@ require("toggleterm").setup()
 vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 
 vim.cmd[[colorscheme tokyonight]]
+
+
+-- Split windows
+vim.api.nvim_set_keymap('n', '<leader>v', ':vsplit<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>s', ':split<CR>', { noremap = true, silent = true })
+
+-- Resize windows
+vim.api.nvim_set_keymap('n', '<leader>+', ':resize +2<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>-', ':resize -2<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>>', ':vertical resize +2<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader><', ':vertical resize -2<CR>', { noremap = true, silent = true })
+
+-- Navigate between windows
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
